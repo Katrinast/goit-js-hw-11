757f1d265d63beeff20bb80923ab35eb756d4e43
Katrinast <stehnovicekaterina@gmail.com>
2024-05-22 23:12:26 +0300

    finished

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -3,10 +3,10 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 
 export const renderImages = (images) => {
   
-return images.map(({ webformatURL, largeImageURL, tags, likes, views,comments, downloads}) => `<a href="${largeImageURL}" class="gallery-item"><img src="${webformatURL}" alt="${tags}"/><div class="info"><p><b>Likes:</b> ${likes}</p>
+return images.map(({ webformatURL, largeImageURL, tags, likes, views,comments, downloads}) => `<li class="gallery-item><a href="${largeImageURL}" class="gallery-link"><img class="gallery-img" src="${webformatURL}" alt="${tags}"/></a><div class="info"><p><b>Likes:</b> ${likes}</p>
         <p><b>Views:</b> ${views}</p>
         <p><b>Comments:</b> ${comments}</p>
-        <p><b>Downloads:</b> ${downloads}</p></div></a>`).join('');
+        <p><b>Downloads:</b> ${downloads}</p></div></li>`).join('');
 }
   
 const lightbox = new SimpleLightbox('.gallery a', {
